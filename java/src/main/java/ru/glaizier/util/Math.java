e02ffd3fcad4c5b05019ebf0e1732586c9a26987
Mikhail Khokhlushin <mkhokhlushin@smile-net.ru>
2016-12-16 17:35:01 +0300

    PowerOfTwo has been moved to Math

--- java/src/main/java/ru/glaizier/util/Math.java
+++ java/src/main/java/ru/glaizier/util/Math.java
@@ -12,5 +12,28 @@ public abstract class Math {
         return true;
     }
 
+    // Can you do this with recursion?
+    public static boolean isPowerOfTwoLogarithmic(int n) {
+        if (n <= 0)
+            throw new IllegalArgumentException();
+
+        // because 2^31 - is negative value
+        double maxNumberToPow = java.lang.Math.pow(2, 30);
+        for (int testValue = 1; testValue < maxNumberToPow; testValue *= 2) {
+            if (testValue == n)
+                return true;
+        }
+        return n == maxNumberToPow;
+    }
+
+    public static boolean isPowerOfTwo(int n) {
+        if (n <= 0)
+            throw new IllegalArgumentException();
+        return (n & n - 1) == 0;
+    }
+
+    public static boolean isPowerOfThree(int n) {
+        return false;
+    }
 
 }
--- java/src/test/java/util/MathTest.java
+++ java/src/test/java/util/MathTest.java
@@ -4,6 +4,8 @@ import org.junit.Assert;
 import org.junit.Test;
 import ru.glaizier.util.Math;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 public class MathTest extends Assert {
 
     @Test
@@ -16,6 +18,35 @@ public class MathTest extends Assert {
         assertFalse(Math.isPrime(1));
         assertFalse(Math.isPrime(16));
         assertFalse(Math.isPrime(6));
+    }
+
+
+    @Test
+    public void testIsPowerOfTwo() {
+        assertTrue(Math.isPowerOfTwo(1));
+        assertTrue(Math.isPowerOfTwo(2));
+        assertFalse(Math.isPowerOfTwo(3));
+        assertFalse(Math.isPowerOfTwo(Integer.MAX_VALUE));
+        assertThatThrownBy(() -> Math.isPowerOfTwoLogarithmic(-1)).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> Math.isPowerOfTwoLogarithmic(0)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @SuppressWarnings("NumericOverflow")
+    public void testIsPowerOfTwoLogarithmic() {
+        assertTrue(Math.isPowerOfTwoLogarithmic(1));
+        assertTrue(Math.isPowerOfTwoLogarithmic(2));
+        assertTrue(Math.isPowerOfTwoLogarithmic(4));
+        assertTrue(Math.isPowerOfTwoLogarithmic(32));
+        assertFalse(Math.isPowerOfTwoLogarithmic(Integer.MAX_VALUE));
+
+        assertThatThrownBy(() -> Math.isPowerOfTwoLogarithmic(-1)).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> Math.isPowerOfTwoLogarithmic(Integer.MAX_VALUE + 1)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+
+    @Test
+    public void testIsPowerOfThree() {
     }
 
 }
