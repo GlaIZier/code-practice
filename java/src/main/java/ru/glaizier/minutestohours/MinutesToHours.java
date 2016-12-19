434407f657fc91d63228e2c175e078719ddc5b82
GlaIZier <kh.mv@bk.ru>
2016-12-19 23:57:30 +0300

    MinutesToHoursTest has been added

---
+++ java/src/main/java/ru/glaizier/minutestohours/MinutesToHours.java
@@ -0,0 +1,14 @@
+package ru.glaizier.minutestohours;
+
+import ru.glaizier.util.Pair;
+
+public class MinutesToHours {
+
+    public static Pair<Integer, Integer> convert(int minutes) {
+        if (minutes < 0)
+            throw new IllegalArgumentException("Minutes should be positive!");
+
+        return null;
+    }
+
+}
