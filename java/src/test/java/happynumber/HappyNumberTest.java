f2ebf8ebf863ccb1e79b544abc1574a4efdacb3f
Mikhail Khokhlushin <mkhokhlushin@smile-net.ru>
2016-12-20 18:29:30 +0300

    HappyNumber has been added

---
+++ java/src/test/java/happynumber/HappyNumberTest.java
@@ -0,0 +1,22 @@
+package happynumber;
+
+import org.junit.Assert;
+import org.junit.Test;
+import ru.glaizier.happynumber.HappyNumber;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class HappyNumberTest extends Assert {
+
+    @Test
+    public void isHappyNumberTest() {
+        assertTrue(HappyNumber.isHappyNumber(7));
+        assertTrue(HappyNumber.isHappyNumber(1));
+        assertFalse(HappyNumber.isHappyNumber(0));
+        assertFalse(HappyNumber.isHappyNumber(2));
+        assertFalse(HappyNumber.isHappyNumber(Integer.MAX_VALUE));
+        assertThatThrownBy(() -> HappyNumber.isHappyNumber(-1)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+
+}
