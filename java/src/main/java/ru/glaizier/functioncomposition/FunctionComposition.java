0a560e79f4bf55a891d15e578c130c2b07be3800
Mikhail Khokhlushin <mkhokhlushin@smile-net.ru>
2016-12-21 16:52:47 +0300

    FunctionComposition has been added

---
+++ java/src/main/java/ru/glaizier/functioncomposition/FunctionComposition.java
@@ -0,0 +1,16 @@
+package ru.glaizier.functioncomposition;
+
+import java.util.List;
+import java.util.function.Function;
+
+public class FunctionComposition {
+
+    public static <T> T compose(List<Function<T, T>> functions, T startValue) {
+        T currentValue = startValue;
+        for (Function<T, T> function : functions) {
+            currentValue = function.apply(currentValue);
+        }
+        return currentValue;
+    }
+
+}
