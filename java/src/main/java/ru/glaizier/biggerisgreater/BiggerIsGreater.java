cdb7eee6c5d4aae50bc6986d36f7a63d22bf087d
GlaIZier <kh.mv@bk.ru>
2016-12-31 00:49:00 +0300

    BiggerIsGreaterTest has been added

---
+++ java/src/main/java/ru/glaizier/biggerisgreater/BiggerIsGreater.java
@@ -0,0 +1,14 @@
+package ru.glaizier.biggerisgreater;
+
+public class BiggerIsGreater {
+
+    /**
+     * https://www.hackerrank.com/challenges/bigger-is-greater
+     * Given a word , rearrange the letters of  to construct another word  in such a way that  is lexicographically greater than . In case of multiple possible answers, find the lexicographically smallest one among them.
+     */
+
+    public static String getSmallestGreater(String s) {
+        return null;
+    }
+
+}
---
+++ java/src/test/java/ru/glaizier/biggerisgreater/BiggerIsGreaterTest.java
@@ -0,0 +1,17 @@
+package ru.glaizier.biggerisgreater;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class BiggerIsGreaterTest extends Assert {
+    @Test
+    public void getSmallestGreater() throws Exception {
+        assertEquals("ba", BiggerIsGreater.getSmallestGreater("ab"));
+        assertEquals("bb", BiggerIsGreater.getSmallestGreater(""));
+        assertEquals("hefg", BiggerIsGreater.getSmallestGreater("hegf"));
+        assertEquals("dkhc", BiggerIsGreater.getSmallestGreater("hcdk"));
+        Assertions.assertThatThrownBy(() -> BiggerIsGreater.getSmallestGreater(null)).isInstanceOf(AssertionError.class);
+    }
+
+}
