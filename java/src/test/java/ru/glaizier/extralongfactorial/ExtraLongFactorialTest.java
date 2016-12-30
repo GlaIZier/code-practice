a6bbe3fdd9df9ddd02cef81c8c43a895c7d507cd
Mikhail Khokhlushin <mkhokhlushin@smile-net.ru>
2016-12-30 12:56:47 +0300

    ExtraLongFactorial has been finished

---
+++ java/src/test/java/ru/glaizier/extralongfactorial/ExtraLongFactorialTest.java
@@ -0,0 +1,25 @@
+package ru.glaizier.extralongfactorial;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.math.BigInteger;
+
+public class ExtraLongFactorialTest extends Assert {
+    @Test
+    public void calculate() throws Exception {
+        Assertions.assertThatThrownBy(() -> ExtraLongFactorial.calculate(0)).isInstanceOf(AssertionError.class);
+        assertEquals(new BigInteger("15511210043330985984000000"), ExtraLongFactorial.calculate(25));
+        assertEquals(new BigInteger("1"), ExtraLongFactorial.calculate(1));
+
+        Assertions.assertThatThrownBy(() -> ExtraLongFactorial.calculateRecursive(0)).isInstanceOf(AssertionError.class);
+        assertEquals(new BigInteger("15511210043330985984000000"), ExtraLongFactorial.calculateRecursive(25));
+        assertEquals(new BigInteger("1"), ExtraLongFactorial.calculateRecursive(1));
+
+        Assertions.assertThatThrownBy(() -> ExtraLongFactorial.calculateTailRecursive(0)).isInstanceOf(AssertionError.class);
+        assertEquals(new BigInteger("15511210043330985984000000"), ExtraLongFactorial.calculateTailRecursive(25));
+        assertEquals(new BigInteger("1"), ExtraLongFactorial.calculateTailRecursive(1));
+    }
+
+}
