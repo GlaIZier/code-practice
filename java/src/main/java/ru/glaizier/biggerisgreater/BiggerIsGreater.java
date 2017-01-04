42b9bdcd14cc942e9b627f03d6194e13341d2fe1
GlaIZier <kh.mv@bk.ru>
2017-01-04 19:47:47 +0300

    BiggerIsGreaterTest has been finished

--- java/src/main/java/ru/glaizier/biggerisgreater/BiggerIsGreater.java
+++ java/src/main/java/ru/glaizier/biggerisgreater/BiggerIsGreater.java
@@ -1,23 +1,83 @@
 package ru.glaizier.biggerisgreater;
 
+import ru.glaizier.util.Pair;
+
 public class BiggerIsGreater {
 
     /**
      * https://www.hackerrank.com/challenges/bigger-is-greater
      * Given a word , rearrange the letters of  to construct another word  in such a way that  is lexicographically greater than . In case of multiple possible answers, find the lexicographically smallest one among them.
      */
-
-    /**
-     * abcde
-     * abced
-     * edcba -> edcab
-     * bdeab
-     */
-
     public static String getSmallestGreater(String s) {
         assert s != null;
 
-        return null;
+        Pair<Integer, Integer> suffix = getLongestSuffix(s);
+        int suffixStart = suffix.getX();
+        int suffixEnd = suffix.getY();
+
+        int pivotIndex = suffixStart - 1;
+
+        if (pivotIndex < 0)
+            return s;
+
+        StringBuilder sb = replaceMinBiggestInSuffixWithPivot(new StringBuilder(s), suffixStart, suffixEnd, pivotIndex);
+
+        return swapSuffix(sb, suffixStart, suffixEnd);
+    }
+
+    private static Pair<Integer, Integer> getLongestSuffix(String s) {
+//        int suffixStart, curSuffixStart, suffixEnd, curSuffixEnd;
+//        suffixStart = curSuffixStart = suffixEnd = curSuffixEnd = s.length() - 1;
+//        for(int i = s.length() - 2; i >= 0; i--) {
+//            // in case of char min is bigger
+//            if (s.charAt(i) >= s.charAt(i + 1))
+//                curSuffixStart = i;
+//            else {
+//                if (curSuffixEnd - curSuffixStart > suffixEnd - suffixStart) {
+//                    suffixEnd = curSuffixEnd;
+//                    suffixStart = curSuffixStart;
+//                }
+//                curSuffixStart = i;
+//                curSuffixEnd = i;
+//            }
+//        }
+//        return (curSuffixEnd - curSuffixStart > suffixEnd - suffixStart) ?
+//                new Pair<>(curSuffixStart, curSuffixEnd) : new Pair<>(suffixStart, suffixEnd);
+        int suffixStart, suffixEnd;
+        suffixStart = suffixEnd = s.length() - 1;
+        for (int i = s.length() - 2; i >= 0; i--) {
+            if (s.charAt(i) >= s.charAt(i + 1))
+                suffixStart = i;
+            else
+                return new Pair<>(suffixStart, suffixEnd);
+        }
+        return new Pair<>(suffixStart, suffixEnd);
+    }
+
+    private static StringBuilder replaceMinBiggestInSuffixWithPivot(StringBuilder sb, int suffixStart, int suffixEnd, int pivotIndex) {
+        int i = suffixEnd;
+        while (sb.charAt(i) <= sb.charAt(pivotIndex) && i >= suffixStart)
+            i--;
+        if (i < suffixStart)
+            throw new IllegalStateException();
+
+        swapChars(sb, pivotIndex, i);
+        return sb;
+    }
+
+    private static void swapChars(StringBuilder sb, int firstIndex, int secondIndex) {
+        char tmp = sb.charAt(firstIndex);
+        sb.setCharAt(firstIndex, sb.charAt(secondIndex));
+        sb.setCharAt(secondIndex, tmp);
+    }
+
+    private static String swapSuffix(StringBuilder sb, int suffixStart, int suffixEnd) {
+        while (suffixEnd > suffixStart) {
+            swapChars(sb, suffixStart, suffixEnd);
+            suffixEnd--;
+            suffixStart++;
+        }
+        return sb.toString();
     }
 
 }
--- java/src/test/java/ru/glaizier/biggerisgreater/BiggerIsGreaterTest.java
+++ java/src/test/java/ru/glaizier/biggerisgreater/BiggerIsGreaterTest.java
@@ -8,9 +8,9 @@ public class BiggerIsGreaterTest extends Assert {
     @Test
     public void getSmallestGreater() throws Exception {
         assertEquals("ba", BiggerIsGreater.getSmallestGreater("ab"));
-        assertEquals("bb", BiggerIsGreater.getSmallestGreater(""));
-        assertEquals("hefg", BiggerIsGreater.getSmallestGreater("hegf"));
-        assertEquals("dkhc", BiggerIsGreater.getSmallestGreater("hcdk"));
+        assertEquals("bb", BiggerIsGreater.getSmallestGreater("bb"));
+        assertEquals("hegf", BiggerIsGreater.getSmallestGreater("hefg"));
+        assertEquals("hcdk", BiggerIsGreater.getSmallestGreater("dkhc"));
         assertEquals("", BiggerIsGreater.getSmallestGreater(""));
         Assertions.assertThatThrownBy(() -> BiggerIsGreater.getSmallestGreater(null)).isInstanceOf(AssertionError.class);
     }
