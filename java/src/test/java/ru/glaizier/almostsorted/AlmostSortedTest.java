cc607ccc6fbf9a52e022d34f203e41573dff7ee2
Mikhail Khokhlushin <mkhokhlushin@smile-net.ru>
2017-01-10 13:53:39 +0300

    AlmostSorted has been finished

--- java/src/test/java/ru/glaizier/almostsorted/AlmostSortedTest.java
+++ java/src/test/java/ru/glaizier/almostsorted/AlmostSortedTest.java
@@ -10,6 +10,8 @@ public class AlmostSortedTest extends Assert {
         assertEquals("swap 0-1", AlmostSorted.canBeSorted(new int[]{4, 2}));
         assertEquals("swap 3-6", AlmostSorted.canBeSorted(new int[]{1, 2, 3, 10, 5, 6, 4, 11}));
         assertEquals("reverse 1-4", AlmostSorted.canBeSorted(new int[]{1, 5, 4, 3, 2, 6}));
+        assertEquals("no", AlmostSorted.canBeSorted(new int[]{2, 15, 10, 20, 12, 30}));
+        assertEquals("no", AlmostSorted.canBeSorted(new int[]{1, 2, 3, 10, 9, 8, 5, 6, 20, 19}));
     }
 
 }
