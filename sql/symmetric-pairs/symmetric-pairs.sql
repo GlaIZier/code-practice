a4944f37f0a0f2f337928a42332e0df9922ef68f
Mikhail Khokhlushin <mkhokhlushin@smile-net.ru>
2017-02-09 09:30:03 +0300

    Symmetric pairs has been finished in sql

--- sql/symmetric-pairs/symmetric-pairs.sql
+++ sql/symmetric-pairs/symmetric-pairs.sql
@@ -1,9 +1,9 @@
 -- https://www.hackerrank.com/challenges/symmetric-pairs
 
-CREATE TABLE pairs(
+CREATE TABLE pairs (
   id SERIAL,
-  x INT NOT NULL,
-  y INT NOT NULL
+  x  INT NOT NULL,
+  y  INT NOT NULL
 );
 
 INSERT INTO pairs (x, y) VALUES
@@ -12,23 +12,39 @@ INSERT INTO pairs (x, y) VALUES
   (20, 21),
   (23, 22),
   (22, 23),
-  (21, 20)
-;
+  (21, 20);
 
 INSERT INTO pairs (x, y) VALUES
-  (20, 20),
-  (20, 20),
   (20, 21),
   (21, 20),
-  (29, 29)
-;
+  (20, 20),
+  (20, 20),
+  (29, 29);
 
 DELETE FROM pairs;
 
 DROP TABLE pairs;
 
 
-SELECT p1.x, p1.y
-FROM pairs p1
-JOIN pairs p2 ON p1.x = p2.y AND p1.y = p2.x AND p1.id != p2.id -- don't need to include the same row
-ORDER BY p1.x, p1.y -- get all symmetric pairs twice
+SELECT
+  x,
+  y
+FROM
+  (SELECT -- get all points twice so need to exclude half
+     row_number()
+     OVER (
+       ORDER BY p1.x, p1.y) AS row_number,
+     p1.x x,
+     p1.y y
+   FROM pairs p1
+     JOIN pairs p2 ON p1.x = p2.y AND p1.y = p2.x AND p1.id != p2.id) sq -- don't need to include the same row;
+WHERE row_number % 2 = 0;
+
+
+-- solution from site
+select f1.x, f1.y from pairs as f1
+  inner join pairs as f2 on f2.y = f1.x
+where f1.y >= f1.x and f2.x = f1.y
+group by f1.x, f1.y
+having count(*) > 1 or (count(*) = 1 and f1.y != f1.x)
+order by f1.x
