33fd20aa6e80b4886d0e16c87e3e270e59ab3229
GlaIZier <kh.mv@bk.ru>
2017-02-20 22:20:08 +0300

    Additional methods have been added to WildcardTest

---
+++ java/src/test/java/ru/glaizier/jdk/CovarianceTest.java
@@ -0,0 +1,19 @@
+package ru.glaizier.jdk;
+
+import org.junit.Test;
+
+public class CovarianceTest {
+
+    @Test
+    public void covarianceTest() {
+        //        List<Object> ll = new ArrayList<Integer>(); // won't compile
+
+        Integer[] ii = {1, 2, 3};
+        Number[] nn = ii;
+//        nn[0] = new Float(23.4); // java.lang.ArrayStoreException: java.lang.Float
+        System.out.println((Integer) nn[0]);
+//        System.out.println((Float) nn[1]); // java.lang.ClassCastException: java.lang.Integer cannot be cast to java.lang.Float
+
+    }
+
+}
--- java/src/test/java/ru/glaizier/jdk/WildcardTest.java
+++ java/src/test/java/ru/glaizier/jdk/WildcardTest.java
@@ -2,7 +2,9 @@ package ru.glaizier.jdk;
 
 import org.junit.Test;
 
+import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 
@@ -11,10 +13,13 @@ public class WildcardTest {
 
     @Test
     public void wildcardTest() {
-        List<? extends Number> l = new ArrayList<Integer>();
-//        List<? extends Number> l = Arrays.asList(5, 6, 7); // but u can't add here anything. Even null
+        List<? extends Number> l = new ArrayList<Double>();
+        l = new ArrayList<Integer>(); // can't add anything here because if the implementation
+//        List<? extends Number> l = Arrays.asList(5, 6, 7); // u can't add here anything. Even null, because asList returns constant list
 
-        // U can't add nothing except null here
+        // U can't add nothing except null here because if u could then eg
+        // List<? extends Serializable> l = new ArrayList<String>();
+        // l.add(5); // in String list we can add 5;
 //        l.add(new Object());
 //        l.add(new Number() {
 //            @Override
@@ -45,6 +50,54 @@ public class WildcardTest {
         Integer i = (Integer) l.get(0); // can't get Integer from there. U can only cast or get number and Object
         Object o = l.get(0);
 //        Double d = (Double) l.get(0); // ClassCastException
+    }
+
+    @Test
+    public void upperBoundTest() {
+        List<? extends Number> l = Arrays.asList(1, 2, 3);
+        Number n = l.get(1);
+        Integer i = (Integer) l.get(1);
+
+//        List<Number> ll = new ArrayList<Integer>(); // can't do it
+        List<? extends Number> lll = new ArrayList<Integer>();
+
+        List<Number> nn = new ArrayList<>();
+        nn.add(new Integer(5));
+        nn.add(new Double(5.1));
+    }
+
+    @Test
+    public void unboundedTest() {
+//        List<Object> o = new ArrayList<Integer>(); // can't do it
+        List<?> u = new ArrayList<Integer>();
+        u = Arrays.asList(1, 2, 3);
+//        u.add(new Object()); // can't do it
+        Object o = u.get(0);
+        Integer i = (Integer) u.get(0);
+    }
+
+    @Test
+    public void lowerBoundTest() {
+        List<? super Integer> l = Arrays.asList(1, 2, 3);
+        l = new ArrayList<Integer>();
+        l = new ArrayList<Number>();
+        l = new ArrayList<Object>();
+
+        // We can add values in here
+        l.add(new Integer(1));
+        Object o = l.get(0);
+        Integer i = (Integer) l.get(0);
+//        i = l.get(0); // can't do it
+//        l.add(new Double(1.1)); // can't do it
+//        l.add(new Object()); // can't do it
+
+
+        List<? super Serializable> s = new ArrayList<Serializable>();
+        s.add("sa");
+        s.add(5);
+        Object o1 = s.get(0);
+        String s1 = (String) s.get(0);
+        Integer i1 = (Integer) s.get(1);
     }
 
 }
