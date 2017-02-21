74c9741e28422162c9f17105ae4469069bafd198
GlaIZier <kh.mv@bk.ru>
2017-02-21 13:04:14 +0300

    WildcardTest has been optimized

--- java/src/test/java/ru/glaizier/jdk/WildcardTest.java
+++ java/src/test/java/ru/glaizier/jdk/WildcardTest.java
@@ -2,9 +2,8 @@ package ru.glaizier.jdk;
 
 import org.junit.Test;
 
-import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 
@@ -12,16 +11,112 @@ import java.util.List;
 public class WildcardTest {
 
     @Test
-    public void wildcardTest() {
-        List<? extends Number> l = new ArrayList<Double>();
-        l = new ArrayList<Integer>(); // can't add anything here because if the implementation
-//        List<? extends Number> l = Arrays.asList(5, 6, 7); // u can't add here anything. Even null, because asList returns constant list
+    public void upperBoundWildcardBriefTest() {
+        List<? extends Number> l = null;
 
-        // U can't add nothing except null here because if u could then eg
-        // List<? extends Serializable> l = new ArrayList<String>();
-        // l.add(5); // in String list we can add 5;
-//        l.add(new Object());
-//        l.add(new Number() {
+        List<? extends Integer> lei = null;
+        List<Integer> li = null;
+        List<Double> ld = null;
+        List<Number> ln = null;
+        l = lei;
+        l = li;
+        l = ld;
+        l = ln;
+
+        List<Object> lo = null;
+        List<?> lu = null;
+        List<? super Number> lsn = null;
+//        l = lo; // can't do it
+//        l = lu; // can't do it
+//        l = lsn; // can't do it
+
+//        l.add(5); // can't do it
+        l = new ArrayList<>();
+        l.add(null);
+        l = Collections.singletonList(1);
+        Number n = l.get(0);
+    }
+
+    @Test
+    public void lowerBoundWildcardBriefTest() {
+        List<? super Number> l = null;
+
+        List<Number> ln = null;
+        List<Object> lo = null;
+        List<? super Object> lso = null;
+        l = ln;
+        l = lo;
+        l = lso;
+
+        List<Integer> li = null;
+        List<Double> ld = null;
+        List<? super Integer> lsi = null;
+        List<?> lu = null;
+        List<? extends Number> len = null;
+//        l = li; // can't do it
+//        l = ld; // can't do it
+//        l = lsi; // can't do it
+//        l = lu; // can't do it
+//        l = len; // can't do it
+
+        l = new ArrayList<>();
+        l.add(5);
+        l.add(5.5);
+        l.add(new Number() {
+            @Override
+            public int intValue() {
+                return 0;
+            }
+
+            @Override
+            public long longValue() {
+                return 0;
+            }
+
+            @Override
+            public float floatValue() {
+                return 0;
+            }
+
+            @Override
+            public double doubleValue() {
+                return 0;
+            }
+        });
+        l.add(null);
+//        l.add(new Object()); // can't do it
+        Object n = l.get(0);
+    }
+
+    @Test
+    public void unBoundedWildcardBriefTest() {
+        List<?> l = null;
+
+        List<? extends Integer> lei = null;
+        List<Integer> li = null;
+        List<Double> ld = null;
+        List<Number> ln = null;
+        List<Object> lo = null;
+        List<? super Number> lsn = null;
+        l = lei;
+        l = li;
+        l = ld;
+        l = ln;
+        l = lo;
+        l = lsn;
+
+//        l.add(5); // can't do it
+        l = new ArrayList<>();
+        l.add(null);
+        l = Collections.singletonList(1);
+        Object n = l.get(0);
+    }
+
+    @Test
+    public void withoutWildcardsBriefTest() {
+        List<Integer> l = new ArrayList<>();
+        l.add(4);
+//        l.add(new Number() { //can't do it
 //            @Override
 //            public int intValue() {
 //                return 0;
@@ -42,71 +137,29 @@ public class WildcardTest {
 //                return 0;
 //            }
 //        });
-//        l.add(5);
-//        l.add(Integer.valueOf(5));
-        l.add(null);
+        List<Number> ln = new ArrayList<>();
+        ln.add(Integer.valueOf(5));
+        ln.add(new Number() {
+            @Override
+            public int intValue() {
+                return 0;
+            }
 
-        Number n = l.get(0);
-        Integer i = (Integer) l.get(0); // can't get Integer from there. U can only cast or get number and Object
-        Object o = l.get(0);
-//        Double d = (Double) l.get(0); // ClassCastException
-    }
+            @Override
+            public long longValue() {
+                return 0;
+            }
 
-    @Test
-    public void upperBoundTest() {
-        List<? extends Number> l = Arrays.asList(1, 2, 3);
-        Number n = l.get(1);
-        Integer i = (Integer) l.get(1);
+            @Override
+            public float floatValue() {
+                return 0;
+            }
 
-//        List<Number> ll = new ArrayList<Integer>(); // can't do it
-        List<? extends Number> lll = new ArrayList<Integer>();
-        List<? extends Integer> llll = new ArrayList<>();
-        lll = llll;
-
-        List<Number> nn = new ArrayList<>();
-        nn.add(new Integer(5));
-        nn.add(new Double(5.1));
-
-        List<? extends Serializable> k = new ArrayList<String>();
-//        k.add(5); // can't do it. That's why u can't add anything in such list except null
-
-        List<? super Object> lo = new ArrayList<Object>();
-        List<?> wl;
-        wl = lo;
-    }
-
-    @Test
-    public void unboundedTest() {
-//        List<Object> o = new ArrayList<Integer>(); // can't do it
-        List<?> u = new ArrayList<Integer>();
-        u = Arrays.asList(1, 2, 3);
-//        u.add(new Object()); // can't do it
-        Object o = u.get(0);
-        Integer i = (Integer) u.get(0);
-    }
-
-    @Test
-    public void lowerBoundTest() {
-        List<? super Integer> l = Arrays.asList(1, 2, 3);
-        l = new ArrayList<Integer>();
-        l = new ArrayList<Number>();
-        l = new ArrayList<Object>();
-
-        // We can add values in here
-        l.add(new Integer(1));
-        Object o = l.get(0);
-        Integer i = (Integer) l.get(0);
-//        i = l.get(0); // can't do it
-//        l.add(new Double(1.1)); // can't do it
-//        l.add(new Object()); // can't do it
-
-
-        List<? super Serializable> s = new ArrayList<Serializable>();
-        s.add("sa");
-        s.add(5);
-        Object o1 = s.get(0);
-        String s1 = (String) s.get(0);
-        Integer i1 = (Integer) s.get(1);
+            @Override
+            public double doubleValue() {
+                return 0;
+            }
+        });
     }
 
 }
