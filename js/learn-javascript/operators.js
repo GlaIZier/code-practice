10409e0966ca6800b9b5f461260ad0fb18b4b4c5
GlaIZier <kh.mv@bk.ru>
2018-02-08 12:14:59 +0300

    js: operators has been updated

--- js/learn-javascript/operators.js
+++ js/learn-javascript/operators.js
@@ -5,3 +5,14 @@ var a = 2;
 var x = 1 + (a *= 2);
 
 assert.equal(x, 5);
+
+// https://learn.javascript.ru/bitwise-operators
+
+function isInteger(x) {
+  return (x ^ 0) === x; // ^ cast to integer implicitly
+}
+
+assert.equal(isInteger(12.5), false);
+assert.equal(isInteger(0), true);
+assert.equal(isInteger(12), true);
+assert.equal(isInteger(-12.4), false);
