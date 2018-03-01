ac6cfb3418277edd18c8162553dd9f5e41387691
GlaIZier <kh.mv@bk.ru>
2018-03-01 11:46:12 +0300

    js: context.js has been updated

--- js/learn-javascript/context.js
+++ js/learn-javascript/context.js
@@ -1,7 +1,7 @@
 var assert = require('assert');
 
 // https://learn.javascript.ru/object-methods
-var calculator = (function() {
+var calculator = (function () {
   var _oper1, _oper2;
 
   return {
@@ -9,7 +9,7 @@ var calculator = (function() {
       _oper1 = oper1;
       _oper2 = oper2;
     },
-    sum: function() {
+    sum: function () {
       return _oper1 + _oper2;
     }
   };
@@ -21,15 +21,15 @@ assert.equal(calculator.sum(), 3);
 
 var ladder = {
   step: 0,
-  up: function() { // вверх по лестнице
+  up: function () { // вверх по лестнице
     this.step++;
     return this;
   },
-  down: function() { // вниз по лестнице
+  down: function () { // вниз по лестнице
     this.step--;
     return this;
   },
-  showStep: function() { // вывести текущую ступеньку
+  showStep: function () { // вывести текущую ступеньку
     return this.step;
   }
 };
@@ -56,15 +56,19 @@ delete obj.valueOf;
 assert.equal(+obj, "42");
 
 // braces are the block of code here. It's equal to [0]
-{}[0]
+{
+}
+[0]
 // braces are the block of code here. It's equal to + {} == NaN
-{} + {}
+{
+}
++{}
 
 var foo = {
-  toString: function() {
+  toString: function () {
     return 'foo';
   },
-  valueOf: function() {
+  valueOf: function () {
     return 2;
   }
 };
@@ -84,32 +88,86 @@ assert.equal(new Date(0) - 0, 0);
 assert.equal(new Array(1)[0] + "", "undefined");
 assert.equal(({})[0], undefined);
 assert.equal([1] + 1, "11");
-assert.equal([1,2] + [3,4], "1,23,4");
+assert.equal([1, 2] + [3, 4], "1,23,4");
 assert.equal([] + null + 1, "null1");
 assert.equal([[0]][0][0], 0);
 assert.equal(isNaN({} + {}), true);
 
-// function sum(a) {
-//   if (!a)
-//     return 0;
-//   return function(b) {
-//     return a + sum(b);
-//   };
-// }
-
-var sum = function(n) {
-  if (!sum.result) {
+// sum of parenthesis of any length
+// my solution
+var sum = function (n) {
+  if (!sum.result)
     sum.result = 0;
-    sum.valueOf = function () {
-      var result = sum.result;
-      // when it's called we need to clear result
-      sum.result = undefined;
-      return result;
-    };
-  }
+
+  sum.valueOf = function () {
+    var result = sum.result;
+    // when it's called we need to clear result
+    sum.result = undefined;
+    return result;
+  };
+
   sum.result += n;
   return sum;
 };
 
+assert.equal(sum(1) == 1, true);
 assert.equal(sum(1)(2)(3) == 6, true);
 assert.equal(sum(1)(2)(3)(-1) == 5, true);
+
+// solution from the website
+var sum1 = function (a) {
+
+  var f = function (b) {
+    a += b;
+    return f;
+  };
+
+  f.valueOf = function () {
+    return a;
+  };
+
+  return f;
+};
+
+// assert.equal(sum1(1) == 1, true);
+assert.equal(sum1(1)(2)(3) == 6, true);
+assert.equal(sum1(1)(2)(3)(-1) == 5, true);
+
+// my solution without valueOf but with empty argument call in the end
+var sum2 = function (n) {
+  if (!sum2.result)
+    sum2.result = 0;
+
+  if (n) {
+    sum2.result += n;
+    return sum2;
+  }
+  else {
+    var result = sum2.result;
+    sum2.result = undefined;
+    return result;
+  }
+};
+
+assert.equal(sum2(1)(), 1);
+assert.equal(sum2(1)(2)(3)(), 6);
+assert.equal(sum2(1)(2)(3)(-1)(), 5);
+
+// https://learn.javascript.ru/constructor-new
+// return the same object by different constructors
+var o = {};
+function A() { return o; }
+function B() { return o; }
+assert.equal(new A, new B);
+
+// calculator
+// Todo finish it
+Calculator = function() {
+
+  this.calculate = function(opActionOp) {
+    opActionOp = opActionOp.trim();
+    var operands = opActionOp.split(" ");
+    console.log(operands);
+  }
+
+};
