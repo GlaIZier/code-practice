994ffb4c53592f9cfd73656f2e1a68846f39da3c
GlaIZier <kh.mv@bk.ru>
2018-03-12 14:26:40 +0300

    js: misc.js has been updated

--- js/learn-javascript/misc.js
+++ js/learn-javascript/misc.js
@@ -1,2 +1,12 @@
 var assert = require('assert');
 
+// https://learn.javascript.ru/class-instanceof
+
+function formatDate(date) { 
+
+}
+
+assert.equal(formatDate('2011-10-02'),  '02.10.11');
+assert.equal(formatDate(1234567890), '14.02.09' );
+assert.equal(formatDate([2014, 0, 1]), '01.01.14');
+assert.equal(formatDate(new Date(2014, 0, 1)), '01.01.14');
