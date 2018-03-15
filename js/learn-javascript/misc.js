c19a02b0497fac2f116ba22b3647f37640304612
GlaIZier <kh.mv@bk.ru>
2018-03-15 12:35:11 +0300

    js: misc.js has been updated

--- js/learn-javascript/misc.js
+++ js/learn-javascript/misc.js
@@ -47,3 +47,77 @@ leader.soldier = soldier.id;
 soldier.leader = leader.id;
 var team = [leader, soldier];
 assert.equal(JSON.stringify(team), '[{"id":1,"name":"Василий Иванович","soldier":2},{"id":2,"name":"Петька","leader":1}]');
+
+// https://learn.javascript.ru/settimeout-setinterval
+// Task 1
+var count = 1;
+var previousCount = 0;
+var timer = setInterval(function() {
+  if (count === 3)
+    clearInterval(timer);
+  count++;
+  previousCount++;
+  assert.equal(previousCount + 1, count);
+}, 10);
+
+// Task 2
+var count1 = 1;
+var previousCount1 = 0;
+var timer1 = setTimeout(function recursiveTimeout() {
+  if (count1 === 3)
+    return;
+  count1++;
+  previousCount1++;
+  assert.equal(previousCount1 + 1, count1);
+  setTimeout(recursiveTimeout, 10);
+}, 10);
+
+// Task 7 delay
+function f(x) {
+  console.log(x);
+  return x;
+}
+function delay(f, millis) {
+  return function (x) {
+    setTimeout(f, millis, x);
+  };
+}
+function webSiteDelay(f, ms) {
+  return function() {
+    var savedThis = this;
+    var savedArgs = arguments;
+    setTimeout(function() {
+      f.apply(savedThis, savedArgs);
+    }, ms);
+  };
+
+}
+
+var f50 = delay(f, 50);
+f50("test");
+
+// Task 8 Debounce
+function debounce(f, delayInMillis) {
+  var timerId;
+  return function() {
+    clearTimeout(timerId);
+    var self = this;
+    var args = arguments;
+    timerId = setTimeout(function () {
+      f.apply(self, args);
+      timerId = null;
+    }, delayInMillis);
+  };
+}
+
+var f = debounce(f, 1000); // it's ok to name wrapper the same name
+
+f(1); // вызов отложен на 1000 мс
+f(2); // предыдущий отложенный вызов игнорируется, текущий (2) откладывается на 1000 мс
+
+// через 1 секунду будет выполнен вызов f(1)
+
+setTimeout( function() { f(3) }, 1100); // через 1100 мс отложим вызов еще на 1000 мс
+setTimeout( function() { f(4) }, 1200); // игнорируем вызов (3)
+
+// через 2200 мс от начала выполнения будет выполнен вызов f(4)
