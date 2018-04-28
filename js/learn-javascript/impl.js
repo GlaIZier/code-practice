16086739e542e061ed51ec4006063572e79f2134
GlaIZier <kh.mv@bk.ru>
2018-04-28 10:34:24 +0300

    js: impl.js has been updated

--- js/learn-javascript/impl.js
+++ js/learn-javascript/impl.js
@@ -5,15 +5,30 @@ let co = require('co');
 /** My implementations for different functions */
 
 /** My generator implementation */
+// Abstraction to hold functions for the generator.
+function CodeGenerator(funcArray) {
+  if (funcArray === undefined || funcArray === null || funcArray.length === 0)
+    throw new Error("Function array is empty!");
+
+  this._funcArray = funcArray;
+}
+
+Object.defineProperty(CodeGenerator.prototype, "funcArray", {
+  get: function funcArray() {
+    return this._funcArray;
+  }
+});
+
+
 function GeneratorResult(value, done) {
   this.value = value;
   this.done = done;
 }
 
-function Generator(funcArray) {
-  if (funcArray === undefined || funcArray === null || funcArray.length === 0)
-    throw new Error("Function array is empty!");
-  this._funcArray = funcArray;
+function Generator(codeGenerator) {
+  if (!(codeGenerator instanceof CodeGenerator))
+    throw new Error("CodeGenerator is needed!");
+  this._funcArray = codeGenerator.funcArray;
   this._nextIndex = 0;
 }
 
@@ -26,7 +41,7 @@ Generator.prototype.next = function (yielded) {
   return new GeneratorResult(value, done);
 };
 
-let myGen = new Generator([() => ":", (arg) => arg + "3", (arg) => console.log(arg)]);
+let myGen = new Generator(new CodeGenerator([() => ":", (arg) => arg + "3", (arg) => console.log(arg)]));
 let firstNext = myGen.next();
 assert.deepEqual(firstNext, {done: false, value: ":"} );
 let secondNext = myGen.next(firstNext.value);
@@ -70,7 +85,7 @@ function _ex(resolve, reject, generator, initialArg) {
       if (newYielded.done === true)
         return resolve(newYieldedValue);
 
-      // if promise then handle with callback
+      // if promise (only promise has standard method then) then handle with callback
       if (newYieldedValue.then) {
         newYieldedValue
           .then(resolvedNewYieldedValue => _exRecur(resolvedNewYieldedValue))
@@ -90,6 +105,12 @@ function _ex(resolve, reject, generator, initialArg) {
 function ex(generator) {
   return new Promise((resolve, reject) => {
     _ex(resolve, reject, generator());
+  });
+}
+
+function exActivated(activatedGenerator) {
+  return new Promise((resolve, reject) => {
+    _ex(resolve, reject, activatedGenerator);
   });
 }
 
@@ -114,4 +135,30 @@ ex(codeGenerator)
   .catch(reject => console.log(reject));
 
 
-// Todo implement co like function: one which uses my generator
+let codeGeneratorArray = [
+  () => {
+  console.log(this);
+    return delayAndReturn("a", 10);
+  },
+  // a got from next
+  (a) => {
+    return delayAndReturn(delayAndReturn(a + "b", 10), 10);
+  },
+  // composition of generators doesn't work as it needs more complicated logic
+  (ab) => delayAndReturn(ab + "c", 10),
+  (abc) => {
+    let abcd = abc + "d";
+    // save abcde to external this.
+    this.abcde = abcd + "e";
+    // Todo this = global context. Bad. Avoid it!
+    return this.abcde;
+  },
+  () => sleep(500),
+  () => this.abcde
+];
+
+let myGenerator = new Generator(new CodeGenerator(codeGeneratorArray));
+
+exActivated(myGenerator)
+  .then(result => {assert.equal(result, "abcde"); console.log(this);})
+  .catch(reject => console.log(reject));
