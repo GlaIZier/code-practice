983e08d24ab6f6807386e38bcbba1e790ee22b0b
GlaIZier <kh.mv@bk.ru>
2018-05-12 13:56:33 +0300

    java: LambdaAvalaibility has been added

---
+++ java/src/main/java/ru/glaizier/codepractice/LambdaAvailability.java
@@ -0,0 +1,24 @@
+package ru.glaizier.codepractice;
+
+import java.util.function.Supplier;
+
+public class LambdaAvailability {
+
+    public String s = "s";
+
+    public int i = 1;
+
+    public static void main(String[] args) {
+        LambdaAvailability la = new LambdaAvailability();
+        String ss = "ss";
+        int ii = 1;
+        Supplier supplier = () -> {
+            la.s  = la.s + "1";
+            la.i = la.i + 1;
+            ii++;
+            ss += "s";
+            return "some";
+        };
+    }
+
+}
