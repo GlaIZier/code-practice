4871233da9a19f97446bfe55ddde551d06d16cfd
GlaIZier <kh.mv@bk.ru>
2018-05-21 11:52:44 +0300

    java: ConcurrencyLimiter has been update

--- java/src/main/java/ru/glaizier/jpoint2018/ok/ConcurrencyLimiter.java
+++ java/src/main/java/ru/glaizier/jpoint2018/ok/ConcurrencyLimiter.java
@@ -7,6 +7,21 @@ public class ConcurrencyLimiter {
     static final int DEFAULT_LIMIT = 10;
     final Semaphore semaphore = new Semaphore(DEFAULT_LIMIT);
 
+    // my additional code
+    private volatile int totalPermits = DEFAULT_LIMIT;
+    private final Object lock = new Object();
+
+    final ConcurrencyLimiter.MySemaphore anotherSemaphore = new ConcurrencyLimiter.MySemaphore(DEFAULT_LIMIT);
+    private class MySemaphore extends Semaphore {
+        private MySemaphore(int permits) {
+            super(permits);
+        }
+        private void decreasePermits(int permits) {
+            this.reducePermits(permits);
+        }
+    }
+    // end of my code
+
     // this method restricts the number of possible calls to some heavy task by the limit
     void runTask(Runnable task) {
         semaphore.acquireUninterruptibly();
@@ -17,9 +32,41 @@ public class ConcurrencyLimiter {
         }
     }
 
-    void setLimit(int newLimit) {
+    void setLimit(int newLimit) throws InterruptedException {
         assert newLimit > 0;
         // implement me
+        if (newLimit == totalPermits) {
+            return;
+        }
+        synchronized (lock) {
+            int delta = newLimit - totalPermits;
+            if (delta > 0) {
+                semaphore.release(delta);
+            } else {
+                if (!semaphore.tryAcquire(Math.abs(delta))) {
+                    throw new RuntimeException("Couldn't decrease permits! Try later...");
+                }
+            }
+            totalPermits = newLimit;
+        }
+    }
+
+    // my another implementation
+    void anotherSetLimit(int newLimit) {
+        assert newLimit > 0;
+        // implement me
+        if (newLimit == totalPermits) {
+            return;
+        }
+        synchronized (lock) {
+            int delta = newLimit - totalPermits;
+            if (delta > 0) {
+                anotherSemaphore.release(delta);
+            } else {
+                anotherSemaphore.decreasePermits(delta);
+            }
+            totalPermits = newLimit;
+        }
     }
 
 }
