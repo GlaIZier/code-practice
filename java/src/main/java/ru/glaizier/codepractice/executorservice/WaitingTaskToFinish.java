61cab21b240fc7152c32e1c485faa4a35a34ac94
GlaIZier <kh.mv@bk.ru>
2018-06-25 11:47:49 +0300

    WaitingTaskToFinish has been fixed

--- java/src/main/java/ru/glaizier/codepractice/executorservice/WaitingTaskToFinish.java
+++ java/src/main/java/ru/glaizier/codepractice/executorservice/WaitingTaskToFinish.java
@@ -2,9 +2,15 @@ package ru.glaizier.codepractice.executorservice;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.*;
-import java.util.concurrent.locks.ReentrantLock;
-import java.util.stream.IntStream;
+import java.util.concurrent.Callable;
+import java.util.concurrent.CompletionService;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorCompletionService;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author GlaIZier
@@ -21,7 +27,8 @@ public class WaitingTaskToFinish {
         return executor.submit(task).get();
     }
 
-    public static <T> Future<? extends T> shutdownAwaitWait(ExecutorService executor, Callable<? extends T> task, int deadlineInSec)
+    public static <T> Future<? extends T> shutdownAwaitWait(ExecutorService executor, Callable<? extends T> task,
+                                                            int deadlineInSec)
         throws InterruptedException {
         Future<? extends T> future = executor.submit(task);
         executor.shutdown();
@@ -50,31 +57,37 @@ public class WaitingTaskToFinish {
         return futures;
     }
 
-    public static <T> List<Future<T>> completionServiceWait(ExecutorService executor, List<? extends Callable<T>> tasks) throws InterruptedException {
+    public static <T> List<Future<T>> completionServiceWait(ExecutorService executor, List<? extends Callable<T>> tasks)
+        throws InterruptedException {
         CompletionService<T> completionService = new ExecutorCompletionService<T>(executor);
         tasks.forEach(completionService::submit);
         ArrayList<Future<T>> futures = new ArrayList<>(tasks.size());
-        for (int i = 0; i < tasks.size(); i++)
+        for (int i = 0; i < tasks.size(); i++) {
             futures.add(completionService.take());
+        }
         return futures;
     }
 
-    public static <T> List<Future<T>> countWait(ExecutorService executor, List<? extends Callable<T>> tasks) throws InterruptedException {
+    public static <T> List<Future<T>> countWait(ExecutorService executor, List<? extends Callable<T>> tasks)
+        throws InterruptedException {
         CountDownLatch lock = new CountDownLatch(1);
         CopyOnWriteArrayList<Exception> exceptions = new CopyOnWriteArrayList<>();
+        CopyOnWriteArrayList<T> results = new CopyOnWriteArrayList<>();
+
         ArrayList<Future<T>> futures = new ArrayList<>(tasks.size());
         new Thread(() -> tasks.forEach(task -> {
             Future<T> future = executor.submit(() -> {
                 try {
-                    System.out.println(futures.size());
-                    return task.call();
+                    T result = task.call();
+                    results.add(result);
+                    return result;
                 } catch (Exception e) {
                     exceptions.add(e);
                     return null;
                 } finally {
-                    System.out.println(futures.size());
-                    if (futures.size() == tasks.size())
+                    if (results.size() + exceptions.size() == tasks.size()) {
                         lock.countDown();
+                    }
                 }
             });
             futures.add(future);
@@ -83,4 +96,4 @@ public class WaitingTaskToFinish {
         return futures;
     }
 
-    }
+}
